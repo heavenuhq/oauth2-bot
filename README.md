2d6d47e0a5d7dcd2d585d98509ec9e719e87c7ff
Pandora <84680499+pandoraLeak@users.noreply.github.com>
2024-03-25 23:16:32 +0100

    .gg/backup

---
+++ README.md
@@ -0,0 +1,2 @@
+# oauth2-bot
+ leak du bot auth de mael levenez jsp quoi jai oublié aka Noxtro le ptit "raideur"
---
+++ config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  "token": "tok3n",
+  "client_secret": "client secret",
+  "mongodb": "url db mongodb",
+  "prefix": "+",
+  "owner": "183657615992029185",
+  "cooldown100": true,
+  "support": "https://discord.gg/backupfr",
+  "client_id": "id du bot",
+  "redirect_uri": "http://127.0.0.1:20010/verify", //met http://127.0.0.1:20010/verify en redirect url sur ton bot
+  "oauth_link": "lien oauth",
+  "owners": [
+    "",
+    "183657615992029185"
+  ],
+  "interval": "1000",
+  "bot_link": "",
+  "emojis": {
+    "nitro": "",
+    "check": "",
+    "arrow": "",
+    "user": "",
+    "user_invite": "",
+    "info": "",
+    "load": "",
+    "error": "",
+    "success": "",
+    "yes": "<",
+    "link": "",
+    "reparer": ""
+  },
+  "webhook": {
+    "avatarURL": "",
+    "URL": "ton url wb",
+    "webhookNAME": "/backupfr"
+  },
+  "purchaseDate": "2023-07-31",
+  "expirationDate": "2023-08-31",
+  "buyerName": "17up_",
+  "botName": "Verify ;#9079",
+  "infobuy": "3€ No Cooldown"
+};
